ae2900a3fb7398b2ca20fd7b31906cd7fc4e0a7e
bidev25 <msree2512@gmail.com>
2018-10-06 11:29:43 -0400

    essage

---
+++ CreatingCalculatorUsingIfElse/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingCalculatorUsingIfElse
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter First Number");
+            string FN = Console.ReadLine();
+            int X = int.Parse(FN);
+            Console.WriteLine("Enter Second Number");
+            string SN = Console.ReadLine();
+            int Y = int.Parse(SN);
+            Console.WriteLine("Choose your operator from (+, -, *, /, %");
+            string Operator = Console.ReadLine();
+            if(Operator == "+")
+            {
+                int Result = X + Y;
+                Console.WriteLine("Resut is {0}", Result);
+                Console.ReadLine();
+            }
+            else if (Operator == "-")
+            {
+                int Result = X - Y;
+                Console.WriteLine("Resut is {0}", Result);
+                Console.ReadLine();
+            }
+            else if (Operator == "*")
+            {
+                int Result = X * Y;
+                Console.WriteLine("Resut is {0}", Result);
+                Console.ReadLine();
+            }
+            else if (Operator == "/")
+            {
+                int Result = X / Y;
+                Console.WriteLine("Resut is {0}", Result);
+                Console.ReadLine();
+            }
+            else if (Operator == "%")
+            {
+                int Result = X % Y;
+                Console.WriteLine("Resut is {0}", Result);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+                Console.ReadLine();
+            }
+        }
+    }
+}
